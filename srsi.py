c100db054106e19a8892626ba041917d40ec1228
jimao <jimao@jimaodeMBP.lan>
2022-04-05 14:52:40 +0800

    数据真实

--- srsi.py
+++ srsi.py
@@ -3,9 +3,11 @@ import pandas as pd
 import pandas_ta as ta
 
 df = pd.read_json("src/BTC_USDT-2h.json")
+df.index = pd.to_datetime(df[0], unit = 'ms')
+df.index.names = ['time']
 df.rename(
   columns = {
-    0: 'time',
+    0: 'offset',
     1: 'open',
     2: 'high',
     3: 'low',
@@ -14,5 +16,13 @@ df.rename(
   },
   inplace = True,
 )
-df.index = pd.to_datetime(df['time'], unit = 'ms')
-print(df)
+df['offset'] = range(len(df.index))
+df.ta.stochrsi(
+  length = 49,
+  rsi_length = 8,
+  k = 8,
+  d = 27,
+  append = True,
+)
+df = df[['offset', 'close', 'STOCHRSIk_49_8_8_27', 'STOCHRSId_49_8_8_27']]
+print(df.head(20))
