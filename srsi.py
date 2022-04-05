9eb513ac4edfdae2c011f2f65c0a9c27099828bd
jimao <jimao@jimaodeMBP.lan>
2022-04-05 14:20:37 +0800

    dataframe正咯

---
+++ srsi.py
@@ -0,0 +1,18 @@
+#!/opt/homebrew/bin/python3
+import pandas as pd
+import pandas_ta as ta
+
+df = pd.read_json("src/BTC_USDT-2h.json")
+df.rename(
+  columns = {
+    0: 'time',
+    1: 'open',
+    2: 'high',
+    3: 'low',
+    4: 'close',
+    5: 'volume',
+  },
+  inplace = True,
+)
+print(df)
+
