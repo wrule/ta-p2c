c6e7d3d1a04f512f59c024b9122fc33d7c96ee40
jimao <jimao@jimaodeMacBook-Pro.local>
2022-04-06 13:59:59 +0800

    调整代码位置

--- src/pioneer.c
+++ src/pioneer.c
@@ -5,7 +5,6 @@
 #include "indicators.h"
 
 int HistLen = 0;
-
 unsigned long * Time;
 double * Open;
 double * High;
@@ -13,6 +12,23 @@ double * Low;
 double * Close;
 double * Volume;
 double * Indexs[1024];
+int StablePoint = 0;
+// 初始资金
+double init_funds = 100.0;
+// 实时资金
+double funds = 0.0;
+// 实时资产
+double assets = 0.0;
+// 费率乘数
+double fee = 0.9985;
+// 上一次购买资金
+double funds_buy = 0.0;
+// 历史最大资金
+double funds_max = 0.0;
+// 盈利交易计数
+int win_count = 0;
+// 亏损交易计数
+int loss_count = 0;
 
 void init_hist() {
   Time = malloc(sizeof(unsigned long) * HistLen);
@@ -80,7 +96,7 @@ void show_ohlcv(int index) {
   );
 }
 
-int StablePoint = 0;
+
 
 void strategy5(
   int rsi_length,
@@ -111,23 +127,6 @@ void strategy5(
 void populate_indicators() {
 
 }
-
-// 初始资金
-double init_funds = 100.0;
-// 实时资金
-double funds = 0.0;
-// 实时资产
-double assets = 0.0;
-// 费率乘数
-double fee = 0.9985;
-// 上一次购买资金
-double funds_buy = 0.0;
-// 历史最大资金
-double funds_max = 0.0;
-// 盈利交易计数
-int win_count = 0;
-// 亏损交易计数
-int loss_count = 0;
 
 /**
  * @brief
