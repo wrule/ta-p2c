182c7b70a4d7ff600671dc01d9c0d5b2cc4a1a3d
jimao <jimao@jimaodeMacBook-Pro.local>
2022-04-06 10:49:23 +0800

    pioneer

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rm hello.so
-gcc -Ofast -shared -o hello.so src/hello.c src/tiamalgamation.c -lta_lib
+rm pioneer.so
+gcc -Ofast -shared -o pioneer.so src/pioneer.c src/tiamalgamation.c -lta_lib
 ./dev.py
