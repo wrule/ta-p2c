fdde4b47a8132fe39023b4f2ff75349e6112da7e
jimao <jimao@jimaodeMacBook-Pro.local>
2022-04-07 12:17:13 +0800

    关闭指标

--- src/pioneer.c
+++ src/pioneer.c
@@ -244,10 +244,10 @@ void indicators(
     StablePoint = k_num;
   }
 
-  printf("%d\n", StablePoint);
-  for (int i = 0; i < 100; ++i) {
-    printf("%d %lf %lf %lf\n", i, Indexs[2][i], Indexs[4][i], Indexs[3][i]);
-  }
+  // printf("%d\n", StablePoint);
+  // for (int i = 0; i < 100; ++i) {
+  //   printf("%d %lf %lf %lf\n", i, Indexs[2][i], Indexs[4][i], Indexs[3][i]);
+  // }
 }
 
 #define X_QUEUE_SIZE 10240
