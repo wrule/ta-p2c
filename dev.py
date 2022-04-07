32bc9045d2f24cc0a1be62d7b55af23dccae5f91
jimao <jimao@jimaodeMacBook-Pro.local>
2022-04-07 16:22:31 +0800

    整理python文件

--- dev.py
+++ dev.py
@@ -3,18 +3,23 @@ from ctypes import *
 import json
 hello = CDLL('./pioneer.so')
 
-hist = json.load(open('src/BTC_USDT-2h.json', 'r'))
-hello.init(len(hist), 10)
+def main():
+  print('数据载入中...')
+  hist = json.load(open('src/BTC_USDT-2h.json', 'r'))
+  print('初始化环境...')
+  hello.init(len(hist), 32)
+  print('填充量价数据...')
+  for index, item in enumerate(hist):
+    hello.set_ohlcv(
+      c_int(index),
+      c_ulong(item[0]),
+      c_double(item[1]),
+      c_double(item[2]),
+      c_double(item[3]),
+      c_double(item[4]),
+      c_double(item[5]),
+    );
+  # hello.find()
+  hello.test()
 
-for index, item in enumerate(hist):
-  hello.set_ohlcv(
-    c_int(index),
-    c_ulong(item[0]),
-    c_double(item[1]),
-    c_double(item[2]),
-    c_double(item[3]),
-    c_double(item[4]),
-    c_double(item[5]),
-  );
-
-hello.find()
+main()
