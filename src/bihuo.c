cee5de2447651a04bc9bcfa4791c84fd444f3bf7
jimao <jimao@jimaodeMBP.lan>
2022-05-11 23:46:17 +0800

    提交

--- src/bihuo.c
+++ src/bihuo.c
@@ -6,7 +6,7 @@ double long_funds = 0.0;
 double short_assets = 0.0;
 double short_funds = 0.0;
 
-double funds = 0.0;
+double funds = 100.0;
 
 void open_long(double amount, double price) {
   long_assets += amount;
@@ -22,13 +22,23 @@ void open_short(double amount, double price) {
 
 void close_long(double price) {
   funds += long_assets * price - long_funds;
+  long_assets = 0.0;
+  long_funds = 0.0;
 }
 
 void close_short(double price) {
   funds += -(short_assets * price - short_funds);
+  short_assets = 0.0;
+  short_funds = 0.0;
 }
 
 int main() {
   printf("你好，世界\n");
+  open_long(100, 10);
+  close_long(11);
+  printf("%lf\n", funds);
+  open_short(100, 10);
+  close_short(8);
+  printf("%lf\n", funds);
   return 0;
 }
