e12bd900a7204f9a2d451c292cdfcca66d421e82
jimao <jimao@jimaodeMBP.lan>
2022-05-11 23:38:46 +0800

    开平

--- src/bihuo.c
+++ src/bihuo.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 
-void to_long(double price, double amount) {
 
+double long_assets = 0.0;
+double long_funds = 0.0;
+double short_assets = 0.0;
+double short_funds = 0.0;
+
+double funds = 0.0;
+
+void open_long(double amount, double price) {
+  long_assets += amount;
+  const double use_funds = amount * price;
+  long_funds += use_funds;
 }
 
-void to_short(double price, double amount) {
+void open_short(double amount, double price) {
+  short_assets += amount;
+  const double use_funds = amount * price;
+  short_funds += use_funds;
+}
 
+void close_long(double price) {
+  funds += long_assets * price - long_funds;
+}
+
+void close_short(double price) {
+  funds += -(short_assets * price - short_funds);
 }
 
 int main() {
