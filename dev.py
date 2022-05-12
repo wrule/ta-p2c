472ae7d269986c13a6a0c744755ec75a8b6a93ab
jimao <jimao@jimaodeMBP.lan>
2022-05-12 23:32:45 +0800

    15m数据

--- dev.py
+++ dev.py
@@ -5,7 +5,7 @@ hello = CDLL('./pioneer.so')
 
 def main():
   print('数据载入中...')
-  hist = json.load(open('src/fBTCUSDT.json', 'r'))
+  hist = json.load(open('src/fBTCUSDT15m.json', 'r'))
   # hist = list(filter(lambda item: item[0] >= 1609459200000, hist));
   print('初始化环境...')
   hello.init(len(hist), 32, c_double(0.9985))
